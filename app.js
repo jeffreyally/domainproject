63076497e65f63088a5aa7efcd7cd24115d961aa
jeffreyally <jeffreyally@gmail.com>
2022-01-10 22:38:48 +0000

    Domain name generator

---
+++ app.js
@@ -0,0 +1,19 @@
+awesomethings = ['IceCream','StimulusChecks','Naps','Popeyes','ThreeDayWeekends','BlanketsFreshOutTheDryer','RedLobsterCheddarBayBiscuits']
+IsandBe = ['is','be']
+WhatIBelieveTheKidsAreSayingTheseDays = ['highkeyavibe','thejam','killingit','daGOAT','straightfire','lowkeyamood','hellagoodnocap',]
+urlextensions = ['.com', '.gov','.edu','.org','.net','.us','.biz','.tv']
+
+for (i = 0; i < awesomethings.length; i++) {
+  for (i1 = 0; i1 < IsandBe.length; i1++) {
+    for (i2 = 0; i2 < WhatIBelieveTheKidsAreSayingTheseDays.length; i2++) {
+      for (i3 = 0; i3 < urlextensions.length; i3++) {
+        console.log(awesomethings[i] + IsandBe[i1] + WhatIBelieveTheKidsAreSayingTheseDays[i2] + urlextensions[i3]
+          )
+
+        }
+
+      }
+
+    }
+
+  }
